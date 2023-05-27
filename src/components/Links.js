2efcb2b0c602efa63f3b93283be1772c8c8e6eb8
raulbarranqueroguerrero <barranquero.raul@gmail.com>
2023-05-27 13:03:44 +0200

    Links margin fix

--- src/components/Links.js
+++ src/components/Links.js
@@ -3,25 +3,31 @@ import { LinkIcon } from "./LinkIcon";
 
 export const Links = () => {
 	return (
-		<div style={{ width: '50%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+		<div style={{
+			width: '50%',
+			display: 'flex',
+			margin: '0 auto',
+			alignItems: 'center',
+			justifyContent: 'center'
+		}}>
 			<LinkIcon
 				href={spotifyPage}
-				alt={'spotify-link'}
+				alt={'spotify-link-icon'}
 				icon={'ri-spotify-fill'}
 			/>
 			<LinkIcon
 				href={instagramLink}
-				alt={'instagram-link'}
+				alt={'instagram-link-icon'}
 				icon={'ri-instagram-fill'}
 			/>
 			<LinkIcon
 				href={youtubeLink}
-				alt={'youtube-link'}
+				alt={'youtube-link-icon'}
 				icon={'ri-youtube-fill'}
 			/>
 			<LinkIcon
 				href={tiktokLink}
-				alt={'tiktok-link'}
+				alt={'tiktok-link-icon'}
 				icon={'ri-tiktok-fill'}
 			/>
 		</div>
